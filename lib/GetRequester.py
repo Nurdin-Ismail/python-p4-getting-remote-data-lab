6d8f3f62c94892e55ce20d14eb586d667c173b62
Nurdin Ismail <nurdin.ismail@student.moringaschool.com>
2023-09-22 14:13:54 +0300

    Completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,20 @@ import requests
 import json
 
 class GetRequester:
-
+    
     def __init__(self, url):
         self.url = url
+        
+        
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        json_load = json.loads(self.get_response_body())
+        return json_load
+    
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
